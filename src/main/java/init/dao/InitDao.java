684908e931f1ca5334a89c88975bf3aea2408867
ThomasKubik <tkubik3@uic.edu>
2021-11-13 19:04:31 -0600

    PD7 step 10 changes

---
+++ src/main/java/init/dao/InitDao.java
@@ -0,0 +1,51 @@
+package init.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import user.domain.User;
+
+
+
+/**
+ * DDL functions performed in database
+ */
+public class InitDao {
+	
+	/**
+	 * user name to connect to the database 
+	 */
+	private String MySQL_user = "aging_population_fitness";  //TODO change user
+	
+	/**
+	 * password of your username to connect to the database
+	 */
+	private String MySQL_password = "1234";  //TODO change password
+	
+
+	
+
+
+	public void initialize() throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aging_population_fitness", MySQL_user, MySQL_password);
+			
+			String sql = "create table test (id int)";
+			PreparedStatement preparestatement = connect.prepareStatement(sql); 
+		    
+		    preparestatement.executeUpdate();
+		    connect.close();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+		
+}
