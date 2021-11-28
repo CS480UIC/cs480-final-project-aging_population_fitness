d8c2c8ade61565bc7968a6f22d3f8693e2df8ddb
aaguad3 <aaguad3@uic.edu>
2021-11-28 16:30:43 -0600

    updated User_profileException

---
+++ src/main/java/user_profile/service/User_profileException.java
@@ -0,0 +1,18 @@
+package user_profile.service;
+
+public class User_profieException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public User_profileException() {
+		super();
+	}
+
+	public User_profileException(String message) {
+		super(message);
+	}
+
+}
