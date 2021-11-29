a2765a5dcd905a7c97af36d2e8fd8adf40dfd66a
aaguad3 <aaguad3@uic.edu>
2021-11-29 12:44:12 -0600

    updated userInjuryDao comments

--- src/main/java/user_injury/dao/User_injuryDao.java
+++ src/main/java/user_injury/dao/User_injuryDao.java
@@ -26,25 +26,28 @@ public class User_injuryDao {
 	 */
 	private String MySQL_password = "1234"; //TODO change password
 
-	public User_injury /*here*/ findByUserID(String user_id) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public User_injury /*here*/ findByInjuryID(String injury_id) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		User_injury user_injury = new User_injury();
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aging_population_fitness", MySQL_user, MySQL_password);
 		    String sql = "select * from user_injury where user_id=?";
 		    PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.setString(1,user_id); /* here */
+		    preparestatement.setString(1,injury_id); /* here */
 		    ResultSet resultSet = preparestatement.executeQuery();
 
 		    while(resultSet.next()){
-		    	String userID = resultSet.getString("user_id");
-		    	System.out.println(user_id);		//testing line
-		    	System.out.println(userID);		//testing line
-		    	if(userID.equals(user_id)){
+		    	String injuryID = resultSet.getString("injury_id");
+		    	System.out.println(injury_id);		//testing line
+		    	System.out.println(injuryID);		//testing line
+		    	if(injuryID.equals(injury_id)){
 		    		System.out.println("theyre equal");
+		    		user_injury.setInjuryID(resultSet.getString("injury_id"));
 		    		user_injury.setUserID(resultSet.getString("user_id"));
-		    		user_injury.setUserName(resultSet.getString("username"));
-		    		user_injury.setInjuryName(resultSet.getString("injury_name"));
+		    		user_injury.setUserInjuryName(resultSet.getString("user_injury_name"));
+		    		user_injury.setUserInjuryDescription(resultSet.getString("user_injury_description"));
+		    		user_injury.setUserInjurySeverity(resultSet.getString("user_injury_severity"));
+		    		user_injury.setUserInjuryDate(resultSet.getString("user_injury_date"));
 		    	}
 		    }
 		    connect.close();
@@ -68,10 +71,13 @@ public class User_injuryDao {
 			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aging_population_fitness", MySQL_user, MySQL_password);
 			
 			/* here */
-			String sql = "insert into user_injury (username, user_injury) values(?,?,?)";
+			String sql = "insert into user_injury (user_id, user_injury_name, user_injury_description, user_injury_severity, user_injury_date) values(?,?,?,?,?)";
 			PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.setString(1,form.getUserName());
-		    preparestatement.setString(2,form.getUserinjury());
+		    preparestatement.setString(1,form.getUserID());
+		    preparestatement.setString(2,form.getUserInjuryName());
+		    preparestatement.setString(3,form.getUserInjuryDescription());
+		    preparestatement.setString(4,form.getUserInjurySeverity());
+		    preparestatement.setString(5,form.getUserInjuryDate());
 		    preparestatement.executeUpdate();
 		    connect.close();
 		} catch(SQLException e) {
@@ -92,11 +98,14 @@ public class User_injuryDao {
 			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aging_population_fitness", MySQL_user, MySQL_password);
 			
 			/* here */
-			String sql = "UPDATE user_injury SET username = ?, user_injury = ? where user_id = ?;";
+			String sql = "UPDATE user_injury SET user_id = ?, user_injury_name = ?, user_injury_description = ?, user_injury_severity = ?, user_injury_date = ? where injury_id = ?;";
 			PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.setString(1,form.getUserName());
-			preparestatement.setString(2,form.getUserinjury());
-		    preparestatement.setString(3,form.getUserID());
+		    preparestatement.setString(1,form.getUserID());
+		    preparestatement.setString(2,form.getUserInjuryName());
+		    preparestatement.setString(3,form.getUserInjuryDescription());
+		    preparestatement.setString(4,form.getUserInjurySeverity());
+		    preparestatement.setString(5,form.getUserInjuryDate());
+		    preparestatement.setString(6,form.getInjuryID());
 		    preparestatement.executeUpdate();
 		    connect.close();
 		} catch(SQLException e) {
@@ -111,17 +120,17 @@ public class User_injuryDao {
 	 * @throws InstantiationException
 	 * @throws IllegalAccessException
 	 */
-	public void delete(String user_id) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public void delete(String injury_id) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aging_population_fitness", MySQL_user, MySQL_password);
 			
 			/* here */
-			String sql = "delete from user_injury where user_id = ?";
+			String sql = "delete from user_injury where injury_id = ?";
 			PreparedStatement preparestatement = connect.prepareStatement(sql); 
-		    preparestatement.setString(1, user_id);
+		    preparestatement.setString(1, injury_id);
 		    System.out.print( "id to delete: ");
-		    System.out.println( user_id);
+		    System.out.println( injury_id);
 		    preparestatement.executeUpdate();
 		    connect.close();
 		} catch(SQLException e) {
