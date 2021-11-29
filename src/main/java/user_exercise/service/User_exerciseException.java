d5b214f186a09c4891ab8f35a6b4ff4720d707cb
aaguad3 <aaguad3@uic.edu>
2021-11-29 13:25:51 -0600

    updated both exception and service exercise

---
+++ src/main/java/user_exercise/service/User_exerciseException.java
@@ -0,0 +1,18 @@
+package user_exercise.service;
+
+public class User_exerciseException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public User_exerciseException() {
+		super();
+	}
+
+	public User_exerciseException(String message) {
+		super(message);
+	}
+
+}
