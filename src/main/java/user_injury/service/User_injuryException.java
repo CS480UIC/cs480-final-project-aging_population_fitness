3c208f7778303eefcbba558b4a7f3ba0e0166784
aaguad3 <aaguad3@uic.edu>
2021-11-28 23:49:50 -0600

    updated User_injuryException

---
+++ src/main/java/user_injury/service/User_injuryException.java
@@ -0,0 +1,18 @@
+package user_injury.service;
+
+public class User_injuryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public User_injuryException() {
+		super();
+	}
+
+	public User_injuryException(String message) {
+		super(message);
+	}
+
+}
