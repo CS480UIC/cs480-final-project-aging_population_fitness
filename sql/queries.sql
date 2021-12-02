17c7936a720db9665c72838d968511034d96ba2c
ThomasKubik <tkubik3@uic.edu>
2021-12-02 16:11:35 -0600

    Update queries.sql
    new queries added

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,9 @@
+CREATE VIEW `average_user_injury_severity_greater_than_three` AS
+SELECT user_id, ROUND( AVG( user_injury_severity) )
+FROM user_injury
+GROUP BY user_id
+HAVING ROUND( AVG( user_injury_severity) ) > 3;
+
 CREATE VIEW `difficulty_greater_than_five` AS
 SELECT * 
 FROM exercise
@@ -8,34 +14,12 @@ SELECT *
 FROM user_injury
 WHERE user_injury_severity >= 5;
 
-CREATE VIEW `average_user_injury_severity_greater_than_five` AS
-SELECT user_id, ROUND( AVG( user_injury_severity) )
-FROM user_injury
-GROUP BY user_id
-HAVING ROUND( AVG( user_injury_severity) ) > 3;
-
-CREATE VIEW `exercise_name_lowercase` AS
-SELECT LOWER(exercise_name), exercise_description
-FROM exercise
-ORDER BY exercise_name;
-
-CREATE VIEW `injury_age_days` AS
-SELECT user_id, user_injury_name, DATEDIFF(DATE(NOW()), user_injury_date)
-FROM user_injury;
-
 CREATE VIEW `profile_names_with_injury_age` AS
 SELECT profile_name, user_injury_name, DATEDIFF(DATE(NOW()), user_injury_date)
 FROM user_injury
 JOIN user_profile
 ON user_injury.user_id = user_profile.user_id;
 
-CREATE VIEW `exercise_difficulty_lessthan_avg` AS
-SELECT exercise_name, exercise_description, exercise_difficulty
-FROM exercise
-WHERE exercise_difficulty < 
-	(SELECT AVG(exercise_difficulty)
-	FROM exercise);
-    
 CREATE VIEW `benefits_of_exercise_with_chest_in_description` AS
 SELECT *
 FROM benefit B
@@ -52,6 +36,36 @@ WHERE 2 <=
 	FROM user_injury
     WHERE user_id = A.user_id
     GROUP BY user_id);
+    
+SELECT username, profile_name, user_injury_name, user_injury_description, user_injury_severity
+FROM user_profile
+JOIN user_injury
+ON user_profile.user_id = user_injury.user_id;
+
+SELECT exercise_name, exercise_description, benefit_name, benefit_description
+FROM exercise
+JOIN benefit
+ON exercise.exercise_id = benefit.exercise_id;
+
+
+
+CREATE VIEW `exercise_name_lowercase` AS
+SELECT LOWER(exercise_name), exercise_description
+FROM exercise
+ORDER BY exercise_name;
+
+CREATE VIEW `injury_age_days` AS
+SELECT user_id, user_injury_name, DATEDIFF(DATE(NOW()), user_injury_date)
+FROM user_injury;
+
+
+
+CREATE VIEW `exercise_difficulty_lessthan_avg` AS
+SELECT exercise_name, exercise_description, exercise_difficulty
+FROM exercise
+WHERE exercise_difficulty < 
+	(SELECT AVG(exercise_difficulty)
+	FROM exercise);
     
 
 
